458ca69cc1bdc2efbc42050aea87e316b704ff3d
Nico Medina <nmedina01@mylangara.ca>
2024-10-31 14:30:53 -0700

    add initial movie gallery

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
 import "./App.css";
+import movies from "../../data/movies.json";
 
 function App() {
   return (
     <>
       <h1>Langara TV+++</h1>
+      <div className="grid">
+        {movies.map((movie) => {
+          return (
+            <div key={movie.imdbID} className="cell movie-thumbnail">
+              <img src={movie.Poster} alt={`${movie.Title} Poster`} />
+              {/* <div>{movie.Title}</div> */}
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
